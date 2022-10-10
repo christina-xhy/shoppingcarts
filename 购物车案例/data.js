9f2e9a3d155d058938ece41e1e69604b4af090c4
christina <christinayxh@163.com>
2022-10-11 00:33:41 +0800

    gou wu che

---
+++ 购物车案例/data.js
@@ -0,0 +1,30 @@
+const books = [
+  {
+    id: 1,
+    name: "算法",
+    publishTime: "2006-9",
+    price: 87,
+    count: 1,
+  },
+  {
+    id: 2,
+    name: "艺术",
+    publishTime: "2002-11",
+    price: 23,
+    count: 1,
+  },
+  {
+    id: 3,
+    name: "编程",
+    publishTime: "2033-8",
+    price: 54,
+    count: 1,
+  },
+  {
+    id: 4,
+    name: "代码大全",
+    publishTime: "2093-1",
+    price: 45,
+    count: 1,
+  },
+];
